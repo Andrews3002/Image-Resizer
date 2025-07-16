1a1eab801874ec24ababb6fbc17d98b718b9850d
Andrews3002 <alexangeloandrews@gmail.com>
2025-07-16 09:07:50 -0400

    commit before running >npx electronmon .

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("Hello World")
+const {app, BrowserWindow} = require("electron")
+const path = require("path")
+
+const isDev = process.env.NODE_ENV != 'development'
+const isMac = process.platform === 'darwin'
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        height: 1080,
+        width: isDev ? 1000 : 500
+    })
+
+    // Open devtools if in dev env
+    if(isDev){
+        mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+
+    app.addListener('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createMainWindow()
+        }
+    })
+})
+
+app.addListener('window-all-closed', () => {
+    if(!isMac){
+        app.quit()
+    }
+})
+
