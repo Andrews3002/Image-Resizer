f2b8f0471bba637256a96247dac39bf217264236
Andrews3002 <alexangeloandrews@gmail.com>
2025-07-17 09:49:01 -0400

    form now shows width and height of selected image

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require("electron")
+const {app, BrowserWindow, Menu} = require("electron")
 const path = require("path")
 
 const isDev = process.env.NODE_ENV != 'development'
@@ -11,12 +11,28 @@ function createMainWindow(){
         width: isDev ? 1000 : 500
     })
 
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
     // Open devtools if in dev env
     if(isDev){
         mainWindow.webContents.openDevTools()
     }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+function createAboutWindow(){
+    const aboutWindow = new BrowserWindow({
+        title: "Image Resizer",
+        height: 300,
+        width: 300
+    })
+
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'))
 }
 
 app.whenReady().then(() => {
@@ -28,6 +44,47 @@ app.whenReady().then(() => {
         }
     })
 })
+
+
+// const fileMenuTab = {
+//     label: 'File',
+//     submenu: [
+//         {
+//             label: 'Quit',
+//             click: () => app.quit(),
+//             accelerator: 'CmdOrCtrl+W'
+//         }
+//     ]
+// }
+//
+//OR
+//
+const fileMenuTab = {
+    role: 'fileMenu'
+}
+
+const macAbout = {
+    label: app.name,
+    submenu: [
+        {
+            label: 'About',
+            createAboutWindow
+        }
+    ]
+}
+
+const winAbout = {
+    label: "Help",
+    submenu: [
+        {
+            label: 'About',
+            click: createAboutWindow
+        }
+    ]
+}
+
+//Menu Template
+const menu = [fileMenuTab, ...(isMac ? [macAbout] : [winAbout])]
 
 app.addListener('window-all-closed', () => {
     if(!isMac){
